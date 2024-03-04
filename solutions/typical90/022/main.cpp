48084a3b4d56ac556ac537604a6de946df761156
Kota Sugimori <kota.sugimori@play.jp>
2024-03-05 00:17:10 +0900

    022

--- solutions/typical90/022/main.cpp
+++ solutions/typical90/022/main.cpp
@@ -1,7 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+// x >= yの条件
+ll get_gcd(ll x, ll y) {
+  if (y == 0)
+    return x;
+  else
+    return get_gcd(y, x % y);
+}
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  ll a, b, c;
+  cin >> a >> b >> c;
+  ll tmp = get_gcd(max(a, b), min(a, b));
+  ll gcd = get_gcd(max(tmp, c), min(tmp, c));
+  ll sum = (a / gcd - 1) + (b / gcd - 1) + (c / gcd - 1);
+  cout << sum << endl;
 }
