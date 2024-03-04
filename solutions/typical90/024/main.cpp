66864a95d768e704bf6ff5ea5c558a55eb5d1513
Kota Sugimori <kota.sugimori@play.jp>
2024-03-05 00:37:22 +0900

    024

--- solutions/typical90/024/main.cpp
+++ solutions/typical90/024/main.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  ll n, k;
+  cin >> n >> k;
+  vector<ll> a(n);
+  vector<ll> b(n);
+  for (ll i = 0; i < n; i++)
+    cin >> a.at(i);
+  ll sum = k;
+  for (ll i = 0; i < n; i++) {
+    cin >> b.at(i);
+    sum -= llabs(b.at(i) - a.at(i));
+    if (sum < 0) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  string result = (sum % 2 == 0) ? "Yes" : "No";
+  cout << result << endl;
 }
