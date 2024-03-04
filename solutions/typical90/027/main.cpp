e8d18b60595beb6dff4d99d0a6713a5e1659dcff
Kota Sugimori <kota.sugimori@play.jp>
2024-03-05 00:45:47 +0900

    027

--- solutions/typical90/027/main.cpp
+++ solutions/typical90/027/main.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  ll n;
+  cin >> n;
+  set<string> s;
+  string si;
+  for (ll i = 0; i < n; i++) {
+    cin >> si;
+    if (!s.count(si)) {
+      s.insert(si);
+      cout << i+1 << endl;
+    }
+  }
 }
