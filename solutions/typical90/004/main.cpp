6632072b86fb9e7daaf39f7c4b7a534771fc2d62
Kota Sugimori <kota.sugimori@play.jp>
2024-03-04 23:30:27 +0900

    010

--- solutions/typical90/004/main.cpp
+++ solutions/typical90/004/main.cpp
@@ -1,7 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  ll h, w;
+  cin >> h >> w;
+  vector<vector<ll>> a(h, vector<ll>(w));
+  vector<ll> row_sum(h, 0);
+  vector<ll> col_sum(w, 0);
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      cin >> a.at(i).at(j);
+      row_sum.at(i) += a.at(i).at(j);
+      col_sum.at(j) += a.at(i).at(j);
+    }
+  }
+
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      ll total = row_sum.at(i) + col_sum.at(j) - a.at(i).at(j);
+      if (j == w - 1) {
+        cout << total << endl;
+      } else {
+        cout << total << ' ';
+      }
+    }
+  }
 }
--- solutions/typical90/010/main.cpp
+++ solutions/typical90/010/main.cpp
@@ -1,7 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  ll n, q;
+  cin >> n;
+  vector<ll> sum1(n);
+  vector<ll> sum2(n);
+  ll tmp1 = 0;
+  ll tmp2 = 0;
+  for (int i = 0; i < n; i++) {
+    ll c, p;
+    cin >> c >> p;
+    if (c == 1)
+      tmp1 += p;
+    else
+      tmp2 += p;
+    sum1.at(i) = tmp1;
+    sum2.at(i) = tmp2;
+  }
+
+  cin >> q;
+  for (int i = 0; i < q; i++) {
+    int l, r;
+    cin >> l >> r;
+    if (l == 1) {
+      cout << sum1.at(r - 1) << ' '
+           << sum2.at(r - 1) << endl;
+    } else {
+      cout << sum1.at(r - 1) - sum1.at(l - 2) << ' '
+           << sum2.at(r - 1) - sum2.at(l - 2) << endl;
+    }
+  }
 }
