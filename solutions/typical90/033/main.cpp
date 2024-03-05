17967105e3d2d19afc47455c4d3b568f1987da08
Kota Sugimori <kota.sugimori@play.jp>
2024-03-05 22:16:52 +0900

    問題追加

--- solutions/typical90/033/main.cpp
+++ solutions/typical90/033/main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
-  int a, b;
-  cin >> a >> b;
+  ll h, w;
+  cin >> h >> w;
+  if (h == 1 || w == 1) {
+    cout << h * w << endl;
+    return 0;
+  }
+  cout << ceill(w / 2.0) * ceill(h / 2.0) << endl;
 }
