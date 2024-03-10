1f98cc4fa0673fd0f5cdc73749481a230b9d4333
Kota Sugimori <kota.sugimori@play.jp>
2024-03-10 23:55:09 +0900

    abc332

--- solutions/abc344/a/main.cpp
+++ solutions/abc344/a/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (int i = 0; (i) < (n); ++(i))
+#define rep(i, n) for (int i = 0; (i) < (int)n; ++(i))
 #define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
 #define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
 inline ll in_ll() {
@@ -16,8 +16,20 @@ inline string in_str() {
 }
 
 int main() {
-  string s = in_str();
-  rep(i, s.size()) {
-    
+  string str = in_str();
+  vector<string> splitted;
+  stringstream ss{str};
+  string s;
+  while (getline(ss, s, '|')) {
+    splitted.push_back(s);
   }
+  if (str.at(str.size() - 1) == '|') {
+    splitted.push_back("");
+  }
+
+  cout << splitted.at(0) << splitted.at(2) << "\n";
+
+  // cout << s.substr(0, 1) << "\n";
+  // vector<string> ss = split(s, '|');
+  // cout << ss.at(0) << ss.at(2) << "\n";
 }
